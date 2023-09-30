e4aaf3a437848000c35ca803d452692205b50e8b
Romulo Bordezani <romulo.bordezani@quietplatforms.com>
2023-09-30 14:41:34 -0300

    feature/custom-hooks

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,22 @@
 /* eslint-env node */
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier'
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'prettier'],
   root: true,
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
-    'prettier/prettier': 2
+    'prettier/prettier': 2,
+    'max-len': [
+      'error',
+      {
+        code: 120,
+        tabWidth: 2,
+        ignoreComments: true, //"comments": 80
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true
+      }
+    ]
   }
 };
